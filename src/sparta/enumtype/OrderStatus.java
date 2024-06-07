63f347ebdf778aceae06ac2829b93f9470e7cf86
박시현 <sihyun615@naver.com>
2024-06-07 11:17:28 +0900

    OrderStatus enum 람다 수정

--- src/sparta/enumtype/OrderStatus.java
+++ src/sparta/enumtype/OrderStatus.java
@@ -3,13 +3,13 @@ package sparta.enumtype;
 import java.util.function.Function;
 
 public enum OrderStatus {
-	DELIVERY_COMPLETED(nextStatus -> false),
-	PRODUCT_SHIPPED(nextStatus -> true),
-	PURCHASE_ORDERED(nextStatus -> false),
-	RESHIP(nextStatus -> true),
-	ORDER_RECEIVED(nextStatus -> nextStatus == DELIVERY_COMPLETED || nextStatus == PRODUCT_SHIPPED),
-	PURCHASE_DECIDED(nextStatus -> nextStatus == PURCHASE_ORDERED),
-	EXCHANGE(nextStatus -> nextStatus == PURCHASE_DECIDED || nextStatus == RESHIP);
+	DELIVERY_COMPLETED(nextStatus -> false),  // 배송완료
+	PRODUCT_SHIPPED(nextStatus -> nextStatus == DELIVERY_COMPLETED),  // 상품발송
+	ORDER_RECEIVED(nextStatus -> nextStatus == DELIVERY_COMPLETED || nextStatus == PRODUCT_SHIPPED),  // 주문접수
+	PRODUCT_ORDERED(nextStatus -> nextStatus == ORDER_RECEIVED),  // 상품 주문
+	RESHIP(nextStatus -> nextStatus == DELIVERY_COMPLETED),  // 재발송
+	PURCHASE_DECIDED(nextStatus -> nextStatus == PRODUCT_ORDERED),  // 구매 결정
+	EXCHANGE(nextStatus -> nextStatus == PURCHASE_DECIDED || nextStatus == RESHIP);  //교환
 
 
 	private final Function<OrderStatus, Boolean> expression;
